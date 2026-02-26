dff24ea334a68834a6ab9b766e8055b18bf20418
jwjinn <jwjinn@gmail.com>
2026-02-26 14:41:19 +0900

    폴더 다이어트

--- mcp-frontend/lib/tools/index.ts
+++
@@ -1,9 +0,0 @@
-import { wikipediaQueryTool } from "@/lib/tools/wikipedia-query";
-
-export const workflowTools = {
-	"wikipedia-query": wikipediaQueryTool(),
-};
-
-export const WORKFLOW_TOOLS = Object.keys(workflowTools) as WorkflowToolId[];
-
-export type WorkflowToolId = keyof typeof workflowTools;
